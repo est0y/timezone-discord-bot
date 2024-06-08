3da9fee5ffdcf29fad2e5432621419996c0f104f
est0y <asteoon@gmail.com>
2024-06-08 10:43:36 +0300

    change hashMap on ConcurrentHashMap

--- src/main/java/human/est0y/timezoneDiscordBot/services/data/InMemoryGuildSettingsService.java
+++ src/main/java/human/est0y/timezoneDiscordBot/services/data/InMemoryGuildSettingsService.java
@@ -4,15 +4,15 @@ import human.est0y.timezoneDiscordBot.domain.GuildSettings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class InMemoryGuildSettingsService implements GuildSettingsService {
     
-    private final Map<Long, GuildSettings> guildSettingsById = new HashMap<>();
+    private final Map<Long, GuildSettings> guildSettingsById = new ConcurrentHashMap<>();
     
     @Override
     public Optional<GuildSettings> findById(long id) {
